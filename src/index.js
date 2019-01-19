350376bb6eb98ea59ffc069cdc57d30d8f07e74a
Howard Shelburne <howard.shelburne@gmail.com>
2019-01-19 10:21:42 -0500

    initial state, created store, connected App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import {createStore} from 'redux'
+import {Provider, connect} from 'react-redux'
+
+const initialState = {
+	currentValue: 0,
+	operator: null,
+	previousValue: 0,
+}
+
+function calculator(state = initialState, action) {
+	return state
+}
+
+const store = createStore(calculator)
 
 const App = () =>
   <h1>Howard Instruments 2019</h1>
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function mapStateToProps(state) {
+	return state
+}
+
+const ConnectedApp = connect(mapStateToProps)(App)
+
+ReactDOM.render(<Provider store={store}><ConnectedApp /></Provider>, document.getElementById("root"));
