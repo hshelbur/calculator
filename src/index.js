dc62c45c01aebd85763d27e410442f6d124a9e25
Howard Shelburne <howard.shelburne@gmail.com>
2019-01-19 10:45:08 -0500

    created actions and updated reducer function

--- src/index.js
+++ src/index.js
@@ -4,19 +4,44 @@ import {createStore} from 'redux'
 import {Provider, connect} from 'react-redux'
 
 const initialState = {
-	currentValue: 0,
+	currentValue: "0",
 	operator: null,
 	previousValue: 0,
 }
 
+// actions
+
+const CLEAR = `CLEAR`
+const ENTER_NUMBER = `ENTER_NUMBER`
+
+function clear() {
+	return {type: CLEAR, data: {}}
+}
+
+function enterNumber(number) { 
+	return {type: ENTER_NUMBER, data: {number}}
+}
+
+// reducers
+
 function calculator(state = initialState, action) {
-	return state
+	switch (action.type) {
+		case CLEAR:
+			return initialState
+		case ENTER_NUMBER:
+			return state.currentValue === "0" ? {...state, currentValue: action.data.number} : {...state, currentValue: state.currentValue + action.data.number}
+		default:
+			return state
+	}
 }
 
 const store = createStore(calculator)
 
-const App = () =>
-  <h1>Howard Instruments 2019</h1>
+const App = ({currentValue, operator, previousValue}) => 
+	<React.Fragment>
+		<h1>Howard Instruments 2019</h1>
+		<h2>{currentValue}</h2>
+	</React.Fragment>
 
 function mapStateToProps(state) {
 	return state
